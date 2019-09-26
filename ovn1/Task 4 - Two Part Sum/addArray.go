fc5422a17954e107da311ac1c294c43f0e82db89
aljica <aljica@debian>
2019-09-26 17:51:21 +0200

    format

--- ovn1/Task 4 - Two Part Sum/addArray.go
+++ ovn1/Task 4 - Two Part Sum/addArray.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 // Add adds the numbers in a and sends the result on res.
-func Add(a []int, res chan <- int) {
-    // TODO
-    total := 0
-    for i := 0; i < len(a); i++ {
-      total += a[i]
-    }
-    res <- total
+func Add(a []int, res chan<- int) {
+	// TODO
+	total := 0
+	for i := 0; i < len(a); i++ {
+		total += a[i]
+	}
+	res <- total
 }
 
 func main() {
-    a := []int{1, 2, 3, 4, 5, 6, 7}
-    n := len(a)
-    ch := make(chan int)
-    go Add(a[:n/2], ch)
-    go Add(a[n/2:], ch)
+	a := []int{1, 2, 3, 4, 5, 6, 7}
+	n := len(a)
+	ch := make(chan int)
+	go Add(a[:n/2], ch)
+	go Add(a[n/2:], ch)
 
-    // TODO: Get the subtotals from the channel and print their sum.
+	// TODO: Get the subtotals from the channel and print their sum.
 
-    totalSum := 0
-    for i:=0; i<2; i++ {
-      totalSum += <-ch
-    }
-    fmt.Println(totalSum)
+	totalSum := 0
+	for i := 0; i < 2; i++ {
+		totalSum += <-ch
+	}
+	fmt.Println(totalSum)
 }
