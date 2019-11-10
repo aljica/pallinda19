4c2d5f90e0a33048b4b2221da3d5740fba98b9a9
aljica <aljica@debian>
2019-11-10 11:34:59 +0100

    update alarm without channels!

--- ovn1/Task 3 - Alarm Clock/alarm.go
+++ ovn1/Task 3 - Alarm Clock/alarm.go
@@ -6,40 +6,22 @@ import (
 	"time"
 )
 
-func Remind(text string, delay time.Duration, c chan<- string) {
-	//delay *= time.Second
+func Remind(text string, delay time.Duration) {
 	delay *= time.Hour
-	time.Sleep(delay)
-
-	hr := time.Now().Hour()
-	min := time.Now().Minute()
-	//sec := time.Now().Second()
-	c <- "Klockan är " + strconv.Itoa(hr) + "." + strconv.Itoa(min) /*+ "." + strconv.Itoa(sec)*/ + ": " + text
+	for {
+		time.Sleep(delay)
+		hr := time.Now().Hour()
+		min := time.Now().Minute()
+		fmt.Println("Klockan är " + strconv.Itoa(hr) + "." + strconv.Itoa(min) + ": " + text)
+	}
 }
 
 func main() {
-
-	c1 := make(chan string)
-	c2 := make(chan string)
-	c3 := make(chan string)
-
 	fmt.Println(time.Now())
 
-	go Remind("Dags att äta", 3, c1)
-	go Remind("Dags att arbeta", 8, c2)
-	go Remind("Dags att sova", 24, c3)
+	go Remind("Dags att äta", 3)
+	go Remind("Dags att arbeta", 8)
+	go Remind("Dags att sova", 24)
 
-	for {
-		select {
-		case msg1 := <-c1:
-			fmt.Println(msg1)
-			go Remind("Dags att äta", 3, c1)
-		case msg2 := <-c2:
-			fmt.Println(msg2)
-			go Remind("Dags att arbeta", 8, c2)
-		case msg3 := <-c3:
-			fmt.Println(msg3)
-			go Remind("Dags att sova", 24, c3)
-		}
-	}
+	select {}
 }
